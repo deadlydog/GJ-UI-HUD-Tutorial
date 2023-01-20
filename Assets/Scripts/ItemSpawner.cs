df38e9c179f9ea30950f78db55799f354a2bf305
Taylor <taylor@bitcutter.com>
2023-01-20 10:18:04 -0600

    Colliders and item spawning.

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ItemSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float healthRatio;
+
+    public Vector2 spawnTimeRange;
+    
+    public Vector2 gravityRange;
+
+    public Vector2 spawnYRange;
+    public Vector2 spawnXRange;
+
+    private Coroutine spawnItemsHandler;
+
+    public GameObject fireball;
+    public GameObject heart;
+
+    private void OnEnable()
     {
-        
+        spawnItemsHandler = StartCoroutine(SpawnItemsRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        StopCoroutine(spawnItemsHandler);
+    }
+
+    private IEnumerator SpawnItemsRoutine()
+    {
+        while (true)
+        {
+            float timer = Random.Range(spawnTimeRange.x, spawnTimeRange.y);
+
+            yield return new WaitForSeconds(timer);
+
+            SpawnItem();
+        }
+    }
+    
+    
+    private void SpawnItem()
+    {
+        if (Random.value <= healthRatio)
+        {
+            Instantiate(heart, new Vector3(Random.Range(spawnXRange.x, spawnXRange.y), Random.Range(spawnYRange.x, spawnYRange.y), 0), quaternion.identity);
+        }
+        else
+        {
+            Instantiate(fireball, new Vector3(Random.Range(spawnXRange.x, spawnXRange.y), Random.Range(spawnYRange.x, spawnYRange.y), 0), quaternion.identity);
+        }
     }
 }
