bb4a5fbd3fd26a8ec02a4436d42e5f5ce4f1c54a
Taylor <taylor@bitcutter.com>
2023-01-20 08:04:25 -0600

    Actual initial commit.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private float _moveDirection;
+
+    private Rigidbody2D _rb;
+    private Animator _animator;
+
+    private float _lastRBVelocityX = 0;
+    
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _moveDirection = Input.GetAxis("Horizontal");
+        CheckAnimation();
+        CheckDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.AddForce(new Vector2(_moveDirection, 0) * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void CheckDirection()
+    {
+        if (_lastRBVelocityX <= 0 && _rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        } else if (_lastRBVelocityX >= 0 && _rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    private void CheckAnimation()
+    {
+        _animator.SetLayerWeight(1, Mathf.Clamp(RemapForAnimation(Mathf.Abs(_rb.velocity.x), 0.1f, 3, 0, 1), 0, 1));
+    }
+    
+    
+    private float RemapForAnimation (float value, float from1, float to1, float from2, float to2) {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+}
